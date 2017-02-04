b0bc45fd40f4ff05d296d3b46b772491f5b9b8cd
WickedWitchWarsaw <zuzka.pacak@gmail.com>
2017-02-04 16:38:31 +0100

    Externalised Master Login & email

--- src/main/java/com/wickedwitch/config/ApplicationConfig.java
+++ src/main/java/com/wickedwitch/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.wickedwitch.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,5 +14,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.wickedwitch.backend.persistance.repositories")
 @EntityScan(basePackages = "com.wickedwitch.backend.persistance.domain.backend")
 @EnableTransactionManagement
+@PropertySource("file:///${user.home}/dev/application-common.properties")
 public class ApplicationConfig {
 }
